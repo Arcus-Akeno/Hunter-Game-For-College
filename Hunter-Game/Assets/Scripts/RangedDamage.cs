bbed4b8b4c0ca8628696210a02cc892dacad443f
Arcus4448 <coldicottjacob@gmail.com>
2026-04-24 16:13:14 +0100

    crosshair added with sound

---
+++ Hunter-Game/Assets/Scripts/RangedDamage.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RangedDamage : MonoBehaviour
+{
+   // public float Damage;
+  //  public float BulletRange;
+   // private Transform Player
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Hunter-Game/Assets/Scripts/Teleports.cs
+++ Hunter-Game/Assets/Scripts/Teleports.cs
@@ -3,11 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 public class Teleports : MonoBehaviour
 {
-
+    [SerializeField] private AudioSource TeleportSoundclip;
     public Transform player;
     public Transform StartLocation;
     public Transform Location1;
+    private AudioSource AudioSource;
 
+    
+    private void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
     public void TeleportStart()
     {
         player.transform.position = StartLocation.transform.position;
@@ -18,4 +24,32 @@ public class Teleports : MonoBehaviour
         player.transform.position = Location1.transform.position;
 
     }
+    public KeyCode Teleport1;
+    //left
+    public KeyCode Teleport2;
+    //right
+     void Update()
+     {
+        if (Input.GetKeyDown(Teleport2))
+        {
+            player.transform.position = StartLocation.transform.position;
+
+            
+            AudioSource.Play();
+        }
+
+
+
+
+        if (Input.GetKeyDown(Teleport1))
+        {
+            player.transform.position = Location1.transform.position;
+            
+            AudioSource.Play();
+            
+        }
+        
+        
+     }
+    
 }
